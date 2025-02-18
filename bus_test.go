7440c47499936befdaf5215d44a09c33ddac9ba2
Roman_Myakotin <roman.miakotin@proximaresearch.com>
2025-02-18 11:22:19 +0200

    Initial commit

--- bus_test.go
+++ bus_test.go
@@ -9,9 +9,13 @@ import (
 func ExampleBus() {
 	ctx := context.Background()
 	bus := New(nil, nil)
-	bus.On(ctx, "subject", SubscriberFunc(func(ctx context.Context, notification Notification) {
-		fmt.Println("event:", notification.Subject, notification.Message)
-	}))
+	bus.On(
+		ctx,
+		"subject",
+		SubscriberFunc(func(ctx context.Context, notification Notification) {
+			fmt.Println("event:", notification.Subject, notification.Message)
+		}),
+	)
 	bus.Publish(ctx, Notification{
 		Subject: "subject",
 		Message: "message",
